b9ca9587ffe37946f79257cef11b24a8774c24c9
Tim Mewe <tim.mewe@me.com>
2020-03-24 09:00:54 +0100

    updates build command

--- grebe.rb
+++ grebe.rb
@@ -2,11 +2,11 @@ class Grebe < Formula
   desc "Simplifies the gRPC-Swift development workflow."
   homepage "https://github.com/Apodini/Grebe"
   url "https://github.com/Apodini/Grebe/archive/1.0.0.tar.gz"
-  sha256 "83903f3b2db6b39f2abe1003a0c33fd9f1122db02aafd23e256c2af7a52a5b08"
+  sha256 "911b36788ea1a2eada8cf56d77b0c2c8c39df5515da8c43dee8dbd62aa75e66e"
   
   def install
     system "swift", "build",
-        "--product", "Grebe-CLI",
+        "--product", "grebe",
         "--configuration", "release",
         "--disable-sandbox"
     bin.install '.build/release/grebe'
