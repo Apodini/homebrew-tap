84025511a1e7aa934b3774a634ff3f18929ebdb3
Tim Mewe <57838338+timmewe@users.noreply.github.com>
2020-04-15 13:04:27 +0000

    grebe 0.0.4

--- grebe.rb
+++ grebe.rb
@@ -1,8 +1,8 @@
 class Grebe < Formula
   desc "Simplifies the gRPC-Swift development workflow."
   homepage "https://github.com/Apodini/Grebe"
-  url "https://github.com/Apodini/Grebe/archive/0.0.3.tar.gz"
-  sha256 "8e0f823f528a9ab869e64d66991eeb0b45bf6516dd657b33bb2529affa9c7b9c"
+  url "https://github.com/Apodini/Grebe/archive/0.0.4.tar.gz"
+  sha256 "717a2ef0e26cc79571674a547dd9274f1d5441f24845460502642464a16b6d77"
   
   def install
     system "swift", "build",
